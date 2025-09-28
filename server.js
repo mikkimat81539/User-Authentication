1503ab39a552b3c47977197d5570e0336450eed7
Mikaela Matthews <mikaelamatthews@MacBookAir.attlocal.net>
2025-09-28 16:03:52 -0500

    breakpoint

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const bcrypt = require('bcrypt')
 
-const users = []
+app.use(express.static(__dirname));
 
-app.get('/signUp.html', (req, res) => {
-    res.json(users)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'signUp.html'));
+});
+
+app.listen(3000, () => {
+    console.log("Server is running")
 })
-
-app.listen(3000)
